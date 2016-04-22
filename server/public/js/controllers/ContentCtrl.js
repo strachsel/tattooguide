b23df842ab9b7d34dfdbcc1fee80f46789acff86
Sonya <sonya@Macs-MacBook-Pro-2.local>
2016-04-21 20:52:35 -0700

    added the controller for showing the tattoo details

--- server/public/js/controllers/ContentCtrl.js
+++ server/public/js/controllers/ContentCtrl.js
@@ -1,7 +1,19 @@
 myApp.controller('ContentCtrl', function($scope, $http) {
 
+  $scope.details = {
+    showing: false,
+  };
+
+  $scope.showDetails = function (){
+    $scope.details.showing = true;
+  }
+  $scope.resetSearch = function (){
+    $scope.details.showing = false;
+  }
+
   $http.get('../public/js/data.json')
   .success(function(response){
     $scope.tattoos = response.records;
   });
+
 });
